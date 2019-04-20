23b2f75c323f0409ba8b37eca4da9dd76d3fde4d
Reynier de la Rosa <sirio3mil@gmail.com>
2019-04-21 00:25:25 +0200

    refactoring

--- tests/Helper/SessionHelperTest.php
+++ tests/Helper/SessionHelperTest.php
@@ -2,6 +2,7 @@
 
 namespace CnCTA\Helper;
 
+use limitium\TAPD\CCAuth\CCAuth;
 use PHPUnit\Framework\TestCase;
 
 class SessionHelperTest extends TestCase
@@ -21,12 +22,22 @@ class SessionHelperTest extends TestCase
 
     public function testSetSessionId()
     {
+        $auth = new CCAuth('sirio3mil@gmail.com', 'Imagin@Rey0717', true);
+        $auth->setBasePath(implode(DIRECTORY_SEPARATOR, [
+            dirname(dirname(__DIR__)),
+            'data',
+            'cookies'
+        ]));
+        $session = $auth->getSession();
+        var_dump($session);
+        /*
         $sessionHelper = new SessionHelper('sirio3mil@gmail.com');
         $sessionHelper->login('Imagin@Rey0717');
         $sessionHelper->setSessionId();
         $this->assertNotEmpty($sessionHelper->getSessionId());
         $this->assertNotEmpty($sessionHelper->getReferrer());
         $this->assertNotEmpty($sessionHelper->getSessionUrl());
+        */
     }
 
     public function testSetSessionKey()
